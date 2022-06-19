38c27985e8f11318e6ee4b021852a726fd8972a6
Tim Nico Bauerschmidt <tim.bauerschmidt@gmail.com>
2022-06-19 22:37:31 +0200

    some experiments

--- compressed_sgd.sh
+++ compressed_sgd.sh
@@ -1,25 +1,23 @@
 METHOD=compressed_sgd
 BATCH_SIZE=64
 EPOCHS=1
-NUM_BITS=1
-BINNING=lin
-NUM_WORKERS=1
 SEED=1917
 FULL_EVAL_INTERVAL=-1
-
+NUM_BITS=2
+BINNING=lin
 python3 main.py \
-    --weight_decay 1.0 \
-    --lr 0.01 0.001 0.001 0.0001 \
+    --weight_decay 1.0 0.8 0.7 0.5 0.3 \
+    --lr 0.003 0.001 0.0005 0.0001 \
+    --num_bits ${NUM_BITS} \
+    --binning ${BINNING} \
     --batchwise_evaluation ${FULL_EVAL_INTERVAL} \
-    --exp_name ${METHOD}_B${BATCH_SIZE}_e${EPOCHS}_${NUM_BITS}bits_${BINNING}Binning_${NUM_WORKERS}workers_EvalInterval${FULL_EVAL_INTERVAL}_Seed${SEED} \
+    --exp_name ${METHOD}_B${BATCH_SIZE}_n${NUM_BITS}_l${BINNING}$_e${EPOCHS}_EvalInterval${FULL_EVAL_INTERVAL}_Seed${SEED} \
     --model vanilla_cnn \
     --optimizer ${METHOD} \
     --batch_size ${BATCH_SIZE} \
     --epochs ${EPOCHS} \
-    --num_bits ${NUM_BITS} \
-    --binning ${BINNING} \
-    --num_workers ${NUM_WORKERS} \
+    --rand_zero True \
     --seed ${SEED} \
-    --rand_zero True\
     --dataset MNIST \
-    --dataset_root /home/amin/Documents/projects/SignSGDVariant/datasets/mnist
+    --one_plot_optimizer False \
+    --dataset_root ./
--- sgd.sh
+++ sgd.sh
@@ -1,11 +1,11 @@
-METHOD=sign_sgd
+METHOD=sgd
 BATCH_SIZE=64
 EPOCHS=1
 SEED=1917
 FULL_EVAL_INTERVAL=50
 python3 main.py \
     --weight_decay 1.0 \
-    --lr 0.003 0.001 0.0008 0.0005 0.0001 \
+    --lr 1.0 0.5 0.1 0.05 0.01 \
     --batchwise_evaluation ${FULL_EVAL_INTERVAL} \
     --exp_name ${METHOD}_B${BATCH_SIZE}_e${EPOCHS}_EvalInterval${FULL_EVAL_INTERVAL}_Seed${SEED} \
     --model vanilla_cnn \
