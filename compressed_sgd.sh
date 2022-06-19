9e9a2bb8d7761a34b9e1c9def7a93afc93581b62
Mohammad Amin <56980571+m-parchami@users.noreply.github.com>
2022-06-19 16:23:07 +0200

    added interval to the name

--- compressed_sgd.sh
+++ compressed_sgd.sh
@@ -5,12 +5,13 @@ NUM_BITS=1
 BINNING=lin
 NUM_WORKERS=1
 SEED=1917
+FULL_EVAL_INTERVAL=-1
 
 python3 main.py \
     --weight_decay 1.0 \
     --lr 0.01 0.001 0.001 0.0001 \
-    --batchwise_evaluation -1 \
-    --exp_name ${METHOD}_B${BATCH_SIZE}_e${EPOCHS}_${NUM_BITS}bits_${BINNING}Binning_${NUM_WORKERS}workers_Seed${SEED} \
+    --batchwise_evaluation ${FULL_EVAL_INTERVAL} \
+    --exp_name ${METHOD}_B${BATCH_SIZE}_e${EPOCHS}_${NUM_BITS}bits_${BINNING}Binning_${NUM_WORKERS}workers_EvalInterval${FULL_EVAL_INTERVAL}_Seed${SEED} \
     --model vanilla_cnn \
     --optimizer ${METHOD} \
     --batch_size ${BATCH_SIZE} \
--- sign_sgd.sh
+++ sign_sgd.sh
@@ -2,12 +2,12 @@ METHOD=sign_sgd
 BATCH_SIZE=64
 EPOCHS=1
 SEED=1917
-
+FULL_EVAL_INTERVAL=-1
 python3 main.py \
     --weight_decay 1.0 \
     --lr 0.01 0.001 0.001 0.0001 \
-    --batchwise_evaluation -1 \
-    --exp_name ${METHOD}_B${BATCH_SIZE}_e${EPOCHS}_Seed${SEED} \
+    --batchwise_evaluation ${FULL_EVAL_INTERVAL} \
+    --exp_name ${METHOD}_B${BATCH_SIZE}_e${EPOCHS}_EvalInterval${FULL_EVAL_INTERVAL}_Seed${SEED} \
     --model vanilla_cnn \
     --optimizer ${METHOD} \
     --batch_size ${BATCH_SIZE} \
