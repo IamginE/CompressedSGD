49743bb57523a7bdf01def98612ae4924fef0d8d
Tim Nico Bauerschmidt <tim.bauerschmidt@gmail.com>
2022-06-20 09:55:42 +0200

    plotting for .pkl files

---
+++ plot.py
@@ -0,0 +1,20 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot(metric, files, keys, out):
+    fig, axs = plt.subplots(1, 1, figsize=(10, 8), squeeze=False)
+    for k in range(len(files)):
+        with open(files[k],'rb') as f:
+            histories = pickle.load(f)
+
+        sample_history = histories[keys[k]][metric]
+        x = np.linspace(1, len(sample_history), num = len(sample_history))
+
+        label = str(keys)
+        axs[0, 0].plot(x, histories[keys[k]][metric], label=label)
+        axs[0,0].legend(loc="best")
+
+
+    plt.tight_layout()
+    plt.savefig(out)
