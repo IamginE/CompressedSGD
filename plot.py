3642cb2a1d92cd82f38c2e99da67e07f0ba63e91
Tim Nico Bauerschmidt <tim.bauerschmidt@gmail.com>
2022-06-20 09:59:14 +0200

    better plotting

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(metric, files, keys, out):
+def plot(metric, files, keys, names, out):
     fig, axs = plt.subplots(1, 1, figsize=(10, 8), squeeze=False)
     for k in range(len(files)):
         with open(files[k],'rb') as f:
@@ -11,7 +11,7 @@ def plot(metric, files, keys, out):
         sample_history = histories[keys[k]][metric]
         x = np.linspace(1, len(sample_history), num = len(sample_history))
 
-        label = str(keys)
+        label = names[k] + ' (lr, decay)=' + str(keys[k])
         axs[0, 0].plot(x, histories[keys[k]][metric], label=label)
         axs[0,0].legend(loc="best")
 
