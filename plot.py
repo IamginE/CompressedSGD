f1614eb15a24f69a77a3a84a6a0decc01a885e50
Tim Nico Bauerschmidt <tim.bauerschmidt@gmail.com>
2022-06-26 18:09:36 +0200

    finalizing things

--- plot.py
+++ plot.py
@@ -3,22 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-"""if __name__=='__main__':
-    plot('batch_loss_hist', 
-        files=['./logs/sign_sgd_B64_e1_EvalInterval50_Seed1917_2/histories.pkl',
-        './logs/sign_sgd_B64_e1_EvalInterval50_Seed1917/histories.pkl',
-        './logs/compressed_sgd_B64_n3_llin1_EvalInterval50_Seed1917_4/histories.pkl',
-        './logs/compressed_sgd_B64_n3_llin1_EvalInterval50_Seed1917_4/histories.pkl',
-        ],
-        keys=[(0.0015, 1), (0.001, 1),
-            (0.0007, 0.9), (0.0008, 0.9)
-            ],
-        names=['signSGD lr=0.0015', 'signSGD lr=0.001',
-                'compressedSGD 3bits lr=0.0007, decay=0.9', 'compressedSGD 3bits lr=0.0008, decay=0.9'
-                ],
-        out='cSGD_3bits_better_sign.png',
-        linspace_mul=50)"""
-
 def plot(metric, files, keys, names, out, title, xaxis, yaxis, linspace_mul=1):
     r""""Plots multiple line plots for the specified files and metric in one plot int the output file 'out'.
     Uses the produced dictionaries from trainer.py."""
