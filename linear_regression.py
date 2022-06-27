d78ce65a5959794952813afb3ef1955a33aa476e
Gandook <zamanimehrad@gmail.com>
2022-06-27 14:15:55 +0200

    linear_regression.py supports compressedSGD with linear and exponential binning now

--- linear_regression.py
+++ linear_regression.py
@@ -2,11 +2,10 @@ import os
 import sys
 import numpy as np
 import datetime
-import torch
 
 optml_directory = "OptML-ex06-solution"
 os.chdir(optml_directory)
-sys.path.insert(1, optml_directory)
+sys.path.insert(1, os.getcwd())
 
 from helpers import *
 
@@ -46,7 +45,7 @@ def stochastic_gradient(targets_b, data_A, params_x, batch_size=1):
     :param targets_b: numpy array of size (num_examples)
     :param data_A: numpy array of size (num_examples, num_features)
     :param params_x: compute the mini-batch gradient at these parameters, numpy array of shape (num_features)
-    :param batch_size: integer: number of datapoints to compute the stochastic gradient from
+    :param batch_size: integer: number of data points to compute the stochastic gradient from
 
     :return: gradient, numpy array of shape (num_features)
     """
@@ -63,6 +62,8 @@ def stochastic_gradient_descent(
         max_iters,
         initial_learning_rate,
         only_sign,
+        binning,
+        num_bits,
         rand_zero,
         decreasing_learning_rate=False):
     # Sources: 1. https://github.com/epfml/OptML_course/blob/master/labs/ex06/solution
@@ -76,7 +77,11 @@ def stochastic_gradient_descent(
     :param batch_size: size of the mini-batches
     :param max_iters: integer, number of updates to do
     :param initial_learning_rate: float
-    :param only_sign: boolean, if True, use signSGD, otherwise use SGD
+    :param only_sign: boolean, if True, use signSGD/compressedSGD, otherwise use SGD
+    :param binning: string, if None, use signSGD, if 'lin', use compressedSGD with linear binning, if 'exp',
+    use compressedSGD with exponential binning.
+    :param num_bits: positive integer, number of bits used to differentiate between bins. There would be
+    2**num_bits bins in total.
     :param rand_zero: boolean, if True, replace zero gradients with ±1 (chosen randomly)
     :param decreasing_learning_rate: if set to true, the learning rate should decay as 1 / t
 
@@ -87,6 +92,10 @@ def stochastic_gradient_descent(
     xs = [initial_x]  # parameters after each update
     objectives = []  # loss values after each update
     x = initial_x
+    max_grad = None
+    min_grad = None
+    decay_max = 1.0
+    decay_min = 1.0
 
     for iteration in range(max_iters):
         grad = stochastic_gradient(targets_b, data_A, x, batch_size=batch_size)
@@ -97,8 +106,33 @@ def stochastic_gradient_descent(
 
         # Turning the vector of gradients into a vector of their signs
         if only_sign:
-            # take sign of gradient
-            grad = np.sign(grad)
+            if binning is None:
+                # take sign of gradient
+                grad = np.sign(grad)
+            elif binning == 'lin' or binning == 'exp':
+                if num_bits < 1:
+                    raise ValueError(f"The number of bits should be positive.")
+
+                if max_grad is None:
+                    max_grad = grad
+                else:
+                    max_grad = np.maximum(max_grad * decay_max, grad)
+
+                if min_grad is None:
+                    min_grad = grad
+                else:
+                    min_grad = np.minimum(min_grad * decay_min, grad)
+
+                if binning == 'lin':
+                    grad[grad > 0] = np.ceil((2 ** (num_bits - 1)) * np.divide(grad[grad > 0], max_grad[grad > 0]))
+                    grad[grad < 0] = -np.ceil((2 ** (num_bits - 1)) * np.divide(grad[grad < 0], min_grad[grad < 0]))
+                else:
+                    grad[grad > 0] = np.ceil((2 ** (num_bits - 1)) * np.divide(np.log(grad[grad > 0] + 1),
+                                                                               np.log(max_grad[grad > 0] + 1)))
+                    grad[grad < 0] = -np.ceil((2 ** (num_bits - 1)) * np.divide(np.log(-grad[grad < 0] + 1),
+                                                                                np.log(-min_grad[grad < 0] + 1)))
+            else:
+                raise ValueError(f"The parameter 'binning' should be None, 'lin', or 'exp'.")
 
             # randomise zero gradients to ±1
             if rand_zero:
@@ -119,7 +153,7 @@ def stochastic_gradient_descent(
     return objectives, xs
 
 
-def optimize_with_sgd(data, best_objective, only_sign=False, rand_zero=True):
+def optimize_with_sgd(data, best_objective, only_sign=False, binning=None, num_bits=0, rand_zero=True):
     # Sources: 1. https://github.com/epfml/OptML_course/blob/master/labs/ex06/solution - modified
     #          2. https://github.com/jxbz/signSGD/blob/master/signSGD_zeros.ipynb (for signSGD)
 
@@ -139,10 +173,12 @@ def optimize_with_sgd(data, best_objective, only_sign=False, rand_zero=True):
     start_time = datetime.datetime.now()
     if only_sign:
         sgd_objectives_dec_gamma_mu, sgd_xs_dec_gamma_mu = stochastic_gradient_descent(
-            b, A, x_initial, batch_size, max_iters, gamma0, only_sign, rand_zero, decreasing_learning_rate=False)
+            b, A, x_initial, batch_size, max_iters, gamma0, only_sign, binning, num_bits, rand_zero,
+            decreasing_learning_rate=False)
     else:
         sgd_objectives_dec_gamma_mu, sgd_xs_dec_gamma_mu = stochastic_gradient_descent(
-            b, A, x_initial, batch_size, max_iters, gamma0, only_sign, rand_zero, decreasing_learning_rate=True)
+            b, A, x_initial, batch_size, max_iters, gamma0, only_sign, binning, num_bits, rand_zero,
+            decreasing_learning_rate=True)
     end_time = datetime.datetime.now()
 
     # Print result
@@ -153,7 +189,7 @@ def optimize_with_sgd(data, best_objective, only_sign=False, rand_zero=True):
     print(f"Final value of f(x) - f(x*) = {(sgd_objectives_dec_gamma_mu[-1] - best_objective):.5f}")
 
 
-def optimize_lm(data=(default_A, default_b), optimizer="SGD"):
+def optimize_lm(data=(default_A, default_b), optimizer="SGD", num_bits=0):
     """
         This function trains a simple linear regression model on the given dataset using the chosen
         optimizer under a least-squares loss. It reports how long it takes, and what the final loss is.
@@ -162,11 +198,14 @@ def optimize_lm(data=(default_A, default_b), optimizer="SGD"):
         variables and a column of ones, and b is an n×1 vector containing the values of the dependent
         variable. By default, it uses the dataset provided in the "OptML-ex06-solution" folder (check
         optml_directory to see where it is).
-        :param optimizer: A string indicating what optimizer should be used. It should be one of these three
+        :param optimizer: A string indicating what optimizer should be used. It should be one of these four
         options:
         1. 'SGD' (default): Stochastic Gradient Descent
         2. 'signSGD': The signSGD algorithm in its vanilla form
-        3. 'signSGDplus': The modified version of signSGD
+        3. 'compressedSGDlin': The modified version of signSGD using linear binning
+        4. 'compressedSGDexp': The modified version of signSGD using exponential binning
+        :param num_bits: A positive integer showing the number of bits used to differentiate between bins
+        in compressedSGD. There would be 2**num_bits bins in total.
 
         :return: No output (based on the chosen optimizer, calls the proper function)
     """
@@ -179,15 +218,23 @@ def optimize_lm(data=(default_A, default_b), optimizer="SGD"):
     best_objective = full_objective(b, A, x_star)
 
     if optimizer == "SGD":
-        optimize_with_sgd(data, best_objective, only_sign=False, rand_zero=False)
+        optimize_with_sgd(data, best_objective, only_sign=False, binning=None, num_bits=0, rand_zero=False)
     elif optimizer == "signSGD":
-        optimize_with_sgd(data, best_objective, only_sign=True, rand_zero=True)
-    elif optimizer == "signSGDplus":
-        # TODO
-        return
+        optimize_with_sgd(data, best_objective, only_sign=True, binning=None, num_bits=0, rand_zero=True)
+    elif optimizer == "compressedSGDlin":
+        optimize_with_sgd(data, best_objective, only_sign=True, binning='lin', num_bits=num_bits, rand_zero=True)
+    elif optimizer == "compressedSGDexp":
+        optimize_with_sgd(data, best_objective, only_sign=True, binning='exp', num_bits=num_bits, rand_zero=True)
     else:
         raise ValueError(f"Cannot use {optimizer}; invalid optimizer name!")
 
 
-optimize_lm()
+# Testing
+print("SGD:")
+optimize_lm(optimizer="SGD")
+print("Vanilla signSGD:")
 optimize_lm(optimizer="signSGD")
+print("compressedSGD with linear binning:")
+optimize_lm(optimizer="compressedSGDlin", num_bits=2)
+print("compressedSGD with exponential binning:")
+optimize_lm(optimizer="compressedSGDexp", num_bits=2)
